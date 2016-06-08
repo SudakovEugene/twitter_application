23757b3e24719570d3bc4c6b830530db77f6ff9d
SudakovEugene <SudakovEugene@gmail.com>
2016-06-09 01:11:31 +0300

    Change README

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,58 +1,24 @@
 require 'spec_helper'
 
-=begin
 describe User do
 
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
-  end
+      describe "micropost associations" do
 
-  subject { @user }
+        before { @user.save }
+        let!(:older_micropost) do
+          FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+        end
+        let!(:newer_micropost) do
+          FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+        end
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-
-  it { should be_valid }
-      .
-      .
-      .
-      describe "when password is not present" do
-    before do
-      @user = User.new(name: "Example User", email: "user@example.com",
-                       password: " ", password_confirmation: " ")
-    end
-    it { should_not be_valid }
-  end
-
-  describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
-    it { should_not be_valid }
-  end
-
-
-  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
-
-  describe "return value of authenticate method" do
-    before { @user.save }
-    let(:found_user) { User.find_by(email: @user.email) }
-
-    describe "with valid password" do
-      it { should eq found_user.authenticate(@user.password) }
-    end
-
-    describe "with invalid password" do
-      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-      it { should_not eq user_for_invalid_password }
-      specify { expect(user_for_invalid_password).to be_false }
-    end
+            it "should destroy associated microposts" do
+          microposts = @user.microposts.to_a
+          @user.destroy
+          expect(microposts).not_to be_empty
+          microposts.each do |micropost|
+            expect(Micropost.where(id: micropost.id)).to be_empty
+          end
+        end
+      end
 end
-
-=end
